1a4ca96496d104d858c139642df018ad72d33ce8
testuser <testuser@example.com>
2021-05-05 11:55:30 -0500

    first commit

---
+++ src/main/java/com/app/mytix/MytixApplication.java
@@ -0,0 +1,13 @@
+package com.app.mytix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/mytix/MytixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mytix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
