6bf6bb18bb859bd6314b5878c0fa24f247812907
Your Name <ryan.freese@astontech.com>
2021-05-05 13:42:37 -0500

    messin around

--- src/main/java/com/app/mytix/domain/ticketInfo.java
+++ src/main/java/com/app/mytix/domain/ticketInfo.java
@@ -10,7 +10,7 @@ public class ticketInfo {
     private Integer id;
 
     @Version
-    private Integer version;
+    private Integer versions;
 
     private String date;
     private Double price;
